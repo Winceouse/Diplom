38a8ed0410cfe24b1926a3db4b7d8b554e4c4540
C hashtag <deasmond228@gmail.com>
2023-03-28 15:46:54 +0300

    mail

--- mail.php
+++ mail.php
@@ -5,13 +5,13 @@ $email = $_POST['email'];
 $telephone = $_POST['telephone'];
 $message = $_POST['message'];
 
-
+$subject = "Надання адвокатських послуг";
 
 $mailHeader = "From :".$name."<".$email.">\r\n Tel : ".$telephone."\r\n";
 
 $recipient = "deasmond228@gmail.com";
 
-mail($recipient,"Надання адвокатських послуг",$message,$mailHeader) or die("Error!");
+mail($recipient,$subject,$message,$mailHeader) or die("Error!");
 
 echo"Message send";
 ?>
