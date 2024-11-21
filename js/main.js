31f6fb373d7ee881e79e9027fc2e6e69f1d0a12c
Los Nazar <deasmond228@gmail.com>
2024-11-21 17:02:24 +0200

    uppdate

--- js/main.js
+++ js/main.js
@@ -26,12 +26,18 @@ popupClose.addEventListener("click",closeFBForm);
 submitBtn.addEventListener("mouseover",changeAperanceOfButton1);
 submitBtn.addEventListener("mouseout",changeAperanceOfButton2);
 
-let number = 10;
-let sentense = "Some words";
-let truth = true;
+window.onscroll = function() {
+    const backToTopButton = document.getElementById("backToTop");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
 
-console.log(typeof(number));
-
-console.log(typeof(sentense));
-
-console.log(typeof(truth));
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+}
